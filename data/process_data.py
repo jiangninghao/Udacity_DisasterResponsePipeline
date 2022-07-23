e47f11d3b02a8d3489338804a4e228c80d0b8632
jiangninghao <47676643+jiangninghao@users.noreply.github.com>
2022-07-22 22:32:53 -0400

    Update process_data.py
    corrected - imported full data_process.py script

--- data/process_data.py
+++ data/process_data.py
@@ -56,3 +56,46 @@ def clean_data(df):
     #drop duplicates from the dataframe
     df = df.drop_duplicates(keep=False)
     
+    return df
+
+
+def save_data(df, database_filename):
+    '''
+    engine  point to database location
+    df  upload and replace data into category_message database
+    '''
+    
+    engine = create_engine('sqlite:///' + database_filename) 
+    df = df.to_sql('category_message', engine, index=False, if_exists='replace')
+    
+    return df
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
